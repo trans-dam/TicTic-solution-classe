901057e217aa3cdde92e86916ca798f69b6f3b29
Daniel Schreurs <DanielSchreurs@users.noreply.github.com>
2025-09-29 15:41:16 +0200

    Color, font, size definitions, and logo asset for the Flutter app

---
+++ code/tictic/constants/sizes.dart
@@ -0,0 +1,18 @@
+const kLogoSizeWelcome = 150.0;
+const kVerticalSpacing = 20.0;
+const kTextSliderHeight = 50.0;
+
+const kHorizontalPaddingS = kHorizontalPadding / 2;
+const kVerticalPaddingS = kVerticalPadding / 2;
+
+const kHorizontalPadding = 18.0;
+const kVerticalPadding = 18.0;
+
+const kHorizontalPaddingL = kHorizontalPadding * 2;
+const kVerticalPaddingL = kVerticalPadding * 2;
+
+const kHorizontalPaddingXL = kHorizontalPaddingL * 2;
+const kVerticalPaddingXL = kVerticalPaddingL * 2;
+
+const kHorizontalPaddingXXL = kHorizontalPaddingXL * 2;
+const kVerticalPaddingXXL = kVerticalPaddingXL * 2;
